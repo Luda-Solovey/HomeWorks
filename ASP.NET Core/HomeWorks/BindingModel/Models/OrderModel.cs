ce987449555c70587fedc263787b898701a7fa55
admin <admin@LUDMILA>
2025-06-21 17:19:51 +0300

    ДЗ 07 Model Binding + перейменований проект

---
+++ ASP.NET Core/HomeWorks/BindingModel/Models/OrderModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BindingModel.Models
+{
+    public class OrderModel
+    {
+        public string First { get; set; } = null;
+        public string Second { get; set; } = null; //чи  реба зануляти якщо не nullable тип??? 
+        public double Count { get; set; } 
+
+    }
+}
